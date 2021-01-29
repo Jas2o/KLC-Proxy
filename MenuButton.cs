a250ae7e89a7a0f93f96fa0d4c5acaae0714bd53
jas2o <43341263+Jas2o@users.noreply.github.com>
2021-01-30 00:02:14 +1100

    Jason doesn't know what he's doing.

--- MenuButton.cs
+++ MenuButton.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CustomControls {
+namespace KLCProxy {
     public class MenuButton : Button {
         [DefaultValue(null)]
         public ContextMenuStrip Menu { get; set; }
